1c1fda01cc065ff023c8cac19328f84073d93547
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-03-25 23:14:38 +0000

    Link tor GitHub repository added at the bottom of the project page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import SearchWeather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <SearchWeather />
+      <div class="container">
+        <h1>Weather App</h1>
+        <SearchWeather />
+        <footer>
+          <p>
+            This project was coded by Kimberley Thornton and is {""}
+            <a
+              href="https://github.com/kimberleyjthornton-debug/react-weather-app2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open sourced
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
